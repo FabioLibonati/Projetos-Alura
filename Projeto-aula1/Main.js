b60dd68b90f8d8be2572932d54ac8fbff0a0fe89
Fabio <fabio_libonati@hotmail.com>
2021-03-22 23:29:10 -0300

    Primeiro projeto da Alura

---
+++ Projeto-aula1/Main.js
@@ -0,0 +1,22 @@
+
+// Pega o valor e converte
+function converter() {
+var valorEmDolar = document.getElementById("value").value
+
+document.getElementById("value").value = 0
+
+var valorEmDolarNumero = parseFloat(valorEmDolar)
+
+var valorEmReal = (valorEmDolarNumero * 5.51).toFixed(2)
+
+showValue(valorEmReal)
+}
+
+// Mostra o valor na tela
+function showValue(ValorEmReal) {
+  if (!(valorEmReal === "NaN")){       
+      document.getElementById("showValue").innerHTML = valorEmReal
+} else {
+  document.getElementById("showValue").innerHTML = "O valor Digitado não é um número"
+}
+}
