f17acdcf5f7e15b761be4de4fdbb392e6819497b
Fabio <fabio_libonati@hotmail.com>
2021-03-30 21:21:11 -0300

    Projeto da aula7-30/03/21
    Projeto da aula 7 do dia 30/03/21

---
+++ Projeto_aula7/Main.js
@@ -0,0 +1,766 @@
+var cartaAndroxus = [{
+    nome: 'Androxus',
+    atributos: {
+        ataque: 70,
+        defesa: 75,
+        magia: 60
+    },
+    url: "https://static.wikia.nocookie.net/paladins_gamepedia/images/e/e8/Banner_Androxus.png/revision/latest/scale-to-width-down/275?cb=20191124134137"
+},
+{
+    nome: 'Ash',
+    atributos: {
+        ataque: 65,
+        defesa: 90,
+        magia: 60
+    },
+    url: "https://static.wikia.nocookie.net/paladins_gamepedia/images/b/bd/Banner_Ash.png/revision/latest/scale-to-width-down/275?cb=20191124134133"
+}]
+
+var cartaAtlas = {
+    nome: 'Atlas',
+    atributos: {
+        ataque: 80,
+        defesa: 85,
+        magia: 80
+    },
+    url: "https://static.wikia.nocookie.net/paladins_gamepedia/images/5/55/Banner_Atlas.png/revision/latest/scale-to-width-down/275?cb=20191124134132"
+}
+
+var cartaBarik = {
+    nome: 'Barik',
+    atributos: {
+        ataque: 70,
+        defesa: 65,
+        magia: 70
+    },
+    url: "https://static.wikia.nocookie.net/paladins_gamepedia/images/e/ec/Banner_Barik.png/revision/latest/scale-to-width-down/275?cb=20191124134134"
+}
+
+var cartaBombKing = {
+    nome: 'Bomb King',
+    atributos: {
+        ataque: 85,
+        defesa: 60,
+        magia: 65
+    },
+    url: "https://static.wikia.nocookie.net/paladins_gamepedia/images/c/ca/Banner_Bomb_King.png/revision/latest/scale-to-width-down/275?cb=20191124141352"
+}
+
+var cartaBuck = {
+    nome: 'Buck',
+    atributos: {
+        ataque: 75,
+        defesa: 80,
+        magia: 60
+    },
+    url: "https://static.wikia.nocookie.net/paladins_gamepedia/images/6/6c/Banner_Buck.png/revision/latest/scale-to-width-down/275?cb=20191124141401"
+}
+
+var cartaCassie = {
+    nome: 'Cassie',
+    atributos: {
+        ataque: 85,
+        defesa: 65,
+        magia: 60
+    },
+    url: "https://static.wikia.nocookie.net/paladins_gamepedia/images/9/92/Banner_Cassie.png/revision/latest/scale-to-width-down/275?cb=20191124134136"
+}
+
+var cartaCorvus = {
+    nome: 'Corvus',
+    atributos: {
+        ataque: 65,
+        defesa: 75,
+        magia: 80
+    },
+    url: "https://static.wikia.nocookie.net/paladins_gamepedia/images/6/64/Banner_Corvus.png/revision/latest/scale-to-width-down/275?cb=20200430102734"
+}
+var cartaDredge = {
+    nome: 'Dredge',
+    atributos: {
+        ataque: 85,
+        defesa: 60,
+        magia: 80
+    },
+    url: "https://static.wikia.nocookie.net/paladins_gamepedia/images/4/4e/Banner_Dredge.png/revision/latest/scale-to-width-down/275?cb=20191124141348"
+}
+
+var cartaDrogoz = {
+    nome: 'Drogoz',
+    atributos: {
+        ataque: 85,
+        defesa: 70,
+        magia: 75
+    },
+    url: "https://static.wikia.nocookie.net/paladins_gamepedia/images/6/69/Banner_Drogoz.png/revision/latest/scale-to-width-down/275?cb=20191124141347"
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+        ataque: 75,
+        defesa: 65,
+        magia: 75
+    },
+    url: "https://static.wikia.nocookie.net/paladins_gamepedia/images/e/ee/Banner_Evie.png/revision/latest/scale-to-width-down/275?cb=20191124134126"
+}
+/*  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+
+var cartaEvie = {
+    nome: 'Evie',
+    atributos: {
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+    }
+}
+  
+var cartaMakoa = {
+    nome: 'Makoa',
+    atributos: {
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+    }
+}
+  
+var cartaSkye = {
+    nome: 'Skye',
+    atributos: {
+    ataque: 65,
+    defesa: 85,
+    magia: 70
+    }
+}*/
+
+var cartaMaquina
+var cartaJogador
+var cartas = [cartaAndroxus, cartaAsh, cartaAtlas, cartaBarik, cartaBombKing, cartaBuck, cartaCassie, cartaCorvus, cartaDredge, cartaDrogoz, cartaEvie]
+
+function sortearCarta() {
+    let numeroCartaMaquina = parseInt(Math.random() * 11)
+    cartaMaquina = cartaAndroxus[numeroCartaMaquina]
+
+    let numeroCartaJogador = parseInt(Math.random() * 11)
+    while (numeroCartaJogador == numeroCartaMaquina) {
+        numeroCartaJogador = parseInt(Math.random() * 11)
+    }
+    cartaJogador = cartas[numeroCartaJogador]
+    document.querySelector('#carta').innerHTML = `<div class='player1'><h2>${cartaJogador.nome}</h2><img src="${cartaJogador.url}"><p>Ataque: ${cartaJogador.atributos.ataque}</p><p>Defesa: ${cartaJogador.atributos.defesa}</p><p>Magia: ${cartaJogador.atributos.magia}</p></div>`
+
+    document.querySelector('#btnSortear').disabled = true
+    //document.querySelector('#btnJogar').disabled = false
+
+    exibirOpcoes()
+}
+
+function exibirOpcoes() {
+    let opcoes = document.querySelector('#opcoes')
+    let opcoesTexto = ""
+    for (let atributo in cartaJogador.atributos) {
+        if (atributo == "ataque") {
+            opcoesTexto += `<span>Ataque: </span><a href='#'><img onclick='ataque()' class='ratioImg' src='https://opengameart.org/sites/default/files/Attack.png' alt='Ataque'></a><br>`
+            //opcoesTexto += `<input type='radio' name='atributo' value='${atributo}'><img class='ratioImg' src='https://opengameart.org/sites/default/files/Attack.png'>`
+        } else if (atributo == "defesa") {
+            opcoesTexto += `<span>Defesa: </span><a href='#'><img class='ratioImg'src='https://previews.123rf.com/images/valentint/valentint1506/valentint150602566/41149061-shield-icon-internet-button-on-white-background.jpg' alt='Defesa'></a><br>`
+            //opcoesTexto += `<input type='radio' name='atributo' value='${atributo}'><img class='ratioImg' src='https://previews.123rf.com/images/valentint/valentint1506/valentint150602566/41149061-shield-icon-internet-button-on-white-background.jpg'>`
+
+        } else {
+            opcoesTexto += `<span>Magia: </span><a href='#'><img class='ratioImg' src='https://previews.123rf.com/images/hobbitfoot/hobbitfoot1709/hobbitfoot170900921/86053567-bottle-of-cold-game-icon-of-magic-elixir-interface-for-rpg-or-match3-game-cold-winter-snow-small-var.jpg' alt='Magia'></a>`
+            //opcoesTexto += `<input type='radio' name='atributo' value='${atributo}'><img class='ratioImg' src='https://previews.123rf.com/images/hobbitfoot/hobbitfoot1709/hobbitfoot170900921/86053567-bottle-of-cold-game-icon-of-magic-elixir-interface-for-rpg-or-match3-game-cold-winter-snow-small-var.jpg'>`
+        }
+    }
+    opcoes.innerHTML = opcoesTexto
+}
+
+/*function jogar() {
+  let atributoSelecionado = obtemAtributoSelecionado()
+  
+  if(cartaJogador.atributos[thisatributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+    alert('Você venceu!!!')
+  } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+    alert('Você perdeu!!!')
+  } else {
+    alert('Empatou')
+  }
+  // Criando a carta do jogador 2 no documento html
+  document.querySelector('#carta').innerHTML += `<div class='player2'><h2>${cartaMaquina.nome}</h2><img src="${cartaMaquina.url}"><p>Ataque: ${cartaMaquina.atributos.ataque}</p><p>Defesa: ${cartaMaquina.atributos.defesa}</p><p>Magia: ${cartaMaquina.atributos.magia}</p></div>`
+
+  document.querySelector('#form').innerHTML += `<button type='button' onclick='jogarNovamente()'>Jogar novamente</button>`
+
+  document.querySelector('#btnJogar').disabled = true;
+}*/
+
+/*function obtemAtributoSelecionado() {
+  var radioAtributo = document.querySelectorAll('.ratioImg')
+  alert(radioAtributo + "test")
+  for(let i = 0;i < radioAtributo.length;i++) {
+    if(radioAtributo[i].click()) {
+      return radioAtributo[i].value
+    }
+  }
+}*/
+
+function jogarNovamente() {
+    location.reload()
+}
+
+function ataque() {
+    //let atributoSelecionado = obtemAtributoSelecionado()
+    this.ataqueJogador = cartaJogador.atributos.ataque
+    this.ataqueMaquina = cartaMaquina.atributos.ataque
+
+    if (this.ataqueJogador > this.ataqueMaquina) {
+        alert('Você venceu!!!')
+    } else if (this.ataqueJogador < this.ataqueMaquina) {
+        alert('Você perdeu!!!')
+    } else {
+        alert('Empatou')
+    }
+    novoJogador()
+}
+
+function defesa() {
+    //let atributoSelecionado = obtemAtributoSelecionado()
+    this.defesaJogador = cartaJogador.atributos.defesa
+    this.defesaMaquina = cartaMaquina.atributos.defesa
+
+    if (this.defesaJogador > this.defesaMaquina) {
+        alert('Você venceu!!!')
+    } else if (this.defesaJogador < this.defesaMaquina) {
+        alert('Você perdeu!!!')
+    } else {
+        alert('Empatou')
+    }
+    novoJogador()
+}
+
+function defesa() {
+    //let atributoSelecionado = obtemAtributoSelecionado()
+    this.magiaJogador = cartaJogador.atributos.magia
+    this.magiaMaquina = cartaMaquina.atributos.magia
+
+    if (this.magiaJogador > this.magiaMaquina) {
+        alert('Você venceu!!!')
+    } else if (this.magiaJogador < this.magiaMaquina) {
+        alert('Você perdeu!!!')
+    } else {
+        alert('Empatou')
+    }
+    novoJogador()
+}
+
+function novoJogador() {
+    // Criando a carta do jogador 2 no documento html
+    document.querySelector('#carta').innerHTML += `<div class='player2'><h2>${cartaMaquina.nome}</h2><img src="${cartaMaquina.url}"><p>Ataque: ${cartaMaquina.atributos.ataque}</p><p>Defesa: ${cartaMaquina.atributos.defesa}</p><p>Magia: ${cartaMaquina.atributos.magia}</p></div>`
+
+    document.querySelector('#form').innerHTML += `<button type='button' onclick='jogarNovamente()'>Jogar novamente</button>`
+    //document.querySelector('#btnJogar').disabled = true;
+}
